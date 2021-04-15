d940ac4ef9e381d168bfe8500c75e5e07ef55abc
C <earthhominid@gmail.com>
2021-04-15 11:38:14 -0300

    updated background styling and added font

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+let addition = function(answer) {
+  let x;
+  let y;
+  let answer = x + y;
+  return answer;
+
+}
+
+let subtraction = function() {
+
+}
+
+let division = function() {
+
+}
+
+let multiplicaiton = function() {
+
+}
