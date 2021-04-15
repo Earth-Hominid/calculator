eabf040630273010b7e86ebdb3ce8433e8a091c6
C <earthhominid@gmail.com>
2021-04-15 14:23:12 -0300

    updated javascript to append

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ class Calculator {
   }
 
   clear() {
-
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = undefined
   }
 
   delete() {
@@ -13,10 +15,11 @@ class Calculator {
   }
 
   appendNumber(number) {
-
+    if(number === '.' && this.currentOperand.includes('.')) return
+    this.currentOperand = this.currentOperand.toString() + number.toString()
   }
 
-  chooseOpertation(operate) {
+  chooseOperation(operation) {
 
   }
 
@@ -25,20 +28,35 @@ class Calculator {
   }
 
   updateDisplay() {
+    this.currentOperandTextElement.innerText = this.currentOperand
 
   }
 }
 
 const numberButtons = document.querySelectorAll('[data-number]');
-const operations = document.querySelectorAll('[data-operation');
-const equalsButton = document.querySelector('data-equals');
-const deleteButton = document.querySelector('data-delete');
-const clearButton = document.querySelector('data-clear');
-const previousOperandTextElement = document.querySelector('data-previous-operand');
-const currentOperandTextElement = document.querySelector('data-current-operand');
+const operations = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const clearButton = document.querySelector('[data-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
 
 
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
+  })
+})
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
+  })
+})
 
 /* addition() {
 }
