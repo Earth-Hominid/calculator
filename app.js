cd53de7a28b5c4aebffd654fdd795b998b50cbe1
C <earthhominid@gmail.com>
2021-04-15 12:57:49 -0300

    linking up buttons to javascript

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operations = document.querySelectorAll('[data-operation');
+const equalsButton = document.querySelector('data-equals');
+const deleteButton = document.querySelector('data-delete');
+const clearButton = document.querySelector('data-clear');
+const previousOperandButton = document.querySelector('data-previous-operand');
+const currentOperandButton = document.querySelector('data-current-operand');
 
 
-let addition = function(answer) {
-  let x;
-  let y;
-  let answer = x + y;
-  return answer;
 
+
+/* let addition = function() {
 }
 
 let subtraction = function() {
-
 }
 
 let division = function() {
-
 }
 
 let multiplicaiton = function() {
-
 }
 
 function operate() {
-  
-}
+} */
